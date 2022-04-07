12084e7b8989e9aa9d1ba5506ff04c3a6c8e6b79
com194 <umang.mittal@daffodilsw.com>
2022-04-07 11:03:37 +0530

    Added otp verification

--- Router.js
+++ Router.js
@@ -1,4 +1,4 @@
-import {Text, View, Naviga} from 'react-native';
+import {StatusBar} from 'react-native';
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -8,22 +8,41 @@ import Todo from './components/Todo';
 import Login from './components/login';
 import SignUp from './components/Signup';
 import store from './redux/store';
+import Posts from './components/petsocial/Posts';
 import {Provider} from 'react-redux';
+import RNBootSplash from 'react-native-bootsplash';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import OtpReader from './components/OtpReader';
 
-const stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default class Router extends Component {
   render() {
     return (
-      <NavigationContainer>
+      <NavigationContainer onReady={() => RNBootSplash.hide({fade: true})}>
+        <StatusBar barStyle="light-content" backgroundColor="#ffd97a" />
         <Provider store={store}>
-          <stack.Navigator initialRouteName="Home">
-            <stack.Screen name="Home" component={Home} />
-            <stack.Screen name="Counter" component={Counter} />
-            <stack.Screen name="Todo" component={Todo} />
-            <stack.Screen name="Login" component={Login} />
-            <stack.Screen name="Signup" component={SignUp} />
-          </stack.Navigator>
+          <Drawer.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              drawerType: 'back',
+              drawerIcon: ({focused}) => {
+                if (focused) {
+                  return <Icon name="caret-right" />;
+                } else {
+                  return <Icon name="arrow-right" />;
+                }
+              },
+            }}>
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Todo" component={Todo} />
+            <Drawer.Screen name="Counter" component={Counter} />
+            <Drawer.Screen name="Login" component={Login} />
+            <Drawer.Screen name="Signup" component={SignUp} />
+            <Drawer.Screen name="Posts" component={Posts} />
+            <Drawer.Screen name="OtpReader" component={OtpReader} />
+          </Drawer.Navigator>
         </Provider>
       </NavigationContainer>
     );
